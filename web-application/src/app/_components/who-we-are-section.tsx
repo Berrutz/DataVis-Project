0c6dc9f26fc7fa381c7e73ed072c101b6445e7f5
Alex Valle <alexvalle75@gmail.com>
2024-10-26 16:27:31 +0200

    Fixed navbar glitch + refactor in hook

--- web-application/src/app/_components/who-we-are-section.tsx
+++ web-application/src/app/_components/who-we-are-section.tsx
@@ -1,21 +1,12 @@
 "use client"
 
-import { useInView } from "react-intersection-observer";
-import { useActiveSectionContext } from "../context/active-section-context-provider";
-import { useEffect } from "react";
+import { useSectionInView } from "@/hooks/use-section-in-view"
 
 export default function WhoWeAreSection() {
-  const { setActiveSection } = useActiveSectionContext();
-  const { ref, inView } = useInView({threshold:0.75});
-  
-  useEffect(() => {
-    if (inView) {
-      setActiveSection("Who are we");
-    }
-  }, [inView])
+  var secRef = useSectionInView("Who are we")
 
   return (
-    <section ref={ref} id="who-are-we" className="h-screen flex items-center justify-center bg-red-50">
+    <section ref={secRef} id="who-are-we" className="h-screen flex items-center justify-center bg-red-50">
       <h1 className="text-6xl font-medium font-serif">Who are we</h1>
     </section>
   );
---
+++ web-application/src/hooks/use-section-in-view.ts
@@ -0,0 +1,19 @@
+import {
+  SectionName,
+  useActiveSectionContext,
+} from "@/app/context/active-section-context-provider";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+
+export function useSectionInView(sectionName: SectionName) {
+  const { ref, inView } = useInView({ threshold: 0.75 });
+  const { setActiveSection, timeLastClick } = useActiveSectionContext();
+
+  useEffect(() => {
+    if (inView && Date.now() - timeLastClick > 1000) {
+      setActiveSection(sectionName);
+    }
+  }, [inView, timeLastClick]);
+
+  return ref;
+}
