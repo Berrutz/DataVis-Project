02342e51837bfacfaef38668871f67f24a56918e
Alex Valle <alexvalle75@gmail.com>
2024-12-14 17:05:06 +0100

    Updated assignment 4 page.tsx to use the new general map-switch-container

--- web-application/src/app/assignments/assignment-4/page.tsx
+++ web-application/src/app/assignments/assignment-4/page.tsx
@@ -5,14 +5,13 @@ import {
   ChartBody,
   ChartContainer,
   ChartH1Title,
-  ChartH2Title,
   ChartHeading,
   ChartSection
 } from '../_components/chart-section';
 
 import { useEffect, useState } from 'react';
 import LineChart from './_components/LineChart';
-import MapContainer from './_components/map-switch-container';
+import MapContainer from '@/components/map-switch-container';
 
 export default function Assignment3() {
   const [windowWidth, setWindowWidth] = useState<number>(1200);
@@ -29,25 +28,46 @@ export default function Assignment3() {
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
+  var chartsWidth =
+    windowWidth < 480
+      ? 450
+      : windowWidth < 768
+        ? 600
+        : windowWidth < 1024
+          ? 720
+          : 820;
+
   return (
-    <AssignmentPage title={'Global analysis of CO2 Emissions and Population Density'}>
-      <ChartSection asidename="Line Chart CO2 Emissions" asidekey="line-chart-co2-emissions">
+    <AssignmentPage
+      title={'Global analysis of CO2 Emissions and Population Density'}
+    >
+      <ChartSection
+        asidename="Line Chart CO2 Emissions"
+        asidekey="line-chart-co2-emissions"
+      >
         <ChartHeading>
           <ChartH1Title>CO2 Emissions by Year</ChartH1Title>
-          <p>This chart shows the CO2 emissions by year for different metrics (Min, Max, Avg).</p>
+          <p>
+            This chart shows the CO2 emissions by year for different metrics
+            (Min, Max, Avg).
+          </p>
         </ChartHeading>
         <ChartBody>
-          <ChartContainer asidename="Chart" asidekey="line-chart-co2-emissions-chart">
+          <ChartContainer
+            asidename="Chart"
+            asidekey="line-chart-co2-emissions-chart"
+          >
             <MapContainer
-              width={
-                windowWidth < 480
-                  ? 450
-                  : windowWidth < 768
-                  ? 600
-                  : windowWidth < 1024
-                  ? 720
-                  : 820
-              }
+              components={[
+                {
+                  buttonText: 'Total',
+                  component: <LineChart newWidth={chartsWidth} />
+                },
+                {
+                  buttonText: 'Per Capita',
+                  component: <LineChart newWidth={chartsWidth} />
+                }
+              ]}
             />
           </ChartContainer>
         </ChartBody>
@@ -55,8 +75,3 @@ export default function Assignment3() {
     </AssignmentPage>
   );
 }
-
-
-
-
-
