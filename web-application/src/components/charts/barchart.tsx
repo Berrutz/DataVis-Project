071af6bee8ddc59a7e3ca890bb2b922dd78effc5
Alex Valle <alexvalle75@gmail.com>
2025-01-27 00:26:32 +0100

    Format barchart component

--- web-application/src/components/charts/barchart.tsx
+++ web-application/src/components/charts/barchart.tsx
@@ -2,20 +2,20 @@ import * as d3 from 'd3';
 import { useEffect, useRef } from 'react';
 
 export interface BarChartProps {
-    x: string[];
-    y: number[];
-    width: number;
-    height: number;
-    colorInterpoaltor: ((t: number) => string) | Iterable<string>;
-    yDomainMin?: number;
-    yDomainMax?: number;
-    yLabelsPrefix?: string;
-    yLabelsSuffix?: string;
-    tooltipMapper?: (x: string, y: number) => string;
-    mt?: number;
-    mr?: number;
-    mb?: number;
-    ml?: number;
+  x: string[];
+  y: number[];
+  width: number;
+  height: number;
+  colorInterpoaltor: ((t: number) => string) | Iterable<string>;
+  yDomainMin?: number;
+  yDomainMax?: number;
+  yLabelsPrefix?: string;
+  yLabelsSuffix?: string;
+  tooltipMapper?: (x: string, y: number) => string;
+  mt?: number;
+  mr?: number;
+  mb?: number;
+  ml?: number;
 }
 
 /**
@@ -40,168 +40,168 @@ export interface BarChartProps {
  * @returns The react component
  */
 export default function BarChart({
-    x,
-    y,
-    width,
-    height,
-    colorInterpoaltor,
-    yDomainMin,
-    yDomainMax,
-    yLabelsPrefix,
-    yLabelsSuffix,
-    tooltipMapper,
-    mt,
-    mr,
-    mb,
-    ml
+  x,
+  y,
+  width,
+  height,
+  colorInterpoaltor,
+  yDomainMin,
+  yDomainMax,
+  yLabelsPrefix,
+  yLabelsSuffix,
+  tooltipMapper,
+  mt,
+  mr,
+  mb,
+  ml
 }: BarChartProps) {
-    // The ref of the chart created by d3
-    const svgRef = useRef<SVGSVGElement | null>(null);
+  // The ref of the chart created by d3
+  const svgRef = useRef<SVGSVGElement | null>(null);
 
-    // The ref of the tooltip
-    const tooltipRef = useRef<HTMLDivElement | null>(null);
+  // The ref of the tooltip
+  const tooltipRef = useRef<HTMLDivElement | null>(null);
 
-    useEffect(() => {
-        // Check that x and y data has some data to display
-        if (x.length <= 0 || y.length <= 0) {
-            throw new Error(
-                `X and Y must contains data (current number of sample: x=${x.length} y=${y.length}, expected x > 0 and y > 0)`
-            );
+  useEffect(() => {
+    // Check that x and y data has some data to display
+    if (x.length <= 0 || y.length <= 0) {
+      throw new Error(
+        `X and Y must contains data (current number of sample: x=${x.length} y=${y.length}, expected x > 0 and y > 0)`
+      );
+    }
+
+    // Check that x and y data has the same number of samples
+    if (x.length != y.length) {
+      throw new Error('X and Y data must have the same lenght');
+    }
+
+    // Clear the svg in case of re-rendering
+    d3.select(svgRef.current).selectAll('*').remove();
+
+    // Define the margin (used to make the svg do not clip to the border of the containing div)
+    const margin = {
+      top: mt || 20,
+      right: mr || 0,
+      bottom: mb || 40,
+      left: ml || 30
+    };
+
+    // Define the Y domain
+    var domain = [
+      yDomainMin || Math.min(0, Math.min(...y)),
+      yDomainMax || Math.max(...y)
+    ];
+
+    // The colorscale of the chart
+    const colorScale = d3.scaleSequential(colorInterpoaltor).domain(domain);
+
+    // Define current svg dimension and properties
+    const svg = d3
+      .select(svgRef.current)
+      .attr('width', width)
+      .attr('height', height)
+      .append('g')
+      .attr('transform', `translate(${margin.left},${margin.top})`);
+
+    // Define X and Y scales
+    const xD3 = d3
+      .scaleBand()
+      .domain(x)
+      .range([0, width - margin.left - margin.right])
+      .padding(0.2);
+
+    const yD3 = d3
+      .scaleLinear()
+      .domain(domain)
+      .nice()
+      .range([height - margin.top - margin.bottom, 0]);
+
+    // Define the X axis labels
+    svg
+      .append('g')
+      .attr('transform', `translate(0,${height - margin.top - margin.bottom})`)
+      .call(d3.axisBottom(xD3).tickSize(0))
+      .selectAll('text')
+      .attr('transform', 'rotate(-45)')
+      .style('text-anchor', 'end');
+
+    // Define the Y axis lables
+    var prefix = yLabelsPrefix || '';
+    var suffix = yLabelsSuffix || '';
+    svg
+      .append('g')
+      .call(d3.axisLeft(yD3).tickFormat((y) => `${prefix}${y}${suffix}`))
+      .append('text')
+      .attr('text-anchor', 'end')
+      .attr('fill', 'black')
+      .attr('font-weight', 'bold')
+      .attr('y', -10)
+      .attr('x', -10);
+
+    // Zip the X and Y values together
+    var data = y.map((value, index) => {
+      return {
+        x: x[index],
+        y: value
+      };
+    });
+
+    // Iterate over the data and create the svg bars and tooltips
+    tooltipMapper =
+      tooltipMapper || ((x, y) => `${x}: ${prefix}${y.toString()}${suffix}`);
+
+    svg
+      .selectAll('rect')
+      .data(data)
+      .enter()
+      .append('rect')
+      .attr('x', (d) => xD3(d.x) || 0)
+      .attr('y', (d) => yD3(d.y))
+      .attr('width', xD3.bandwidth())
+      .attr('height', (d) => height - margin.top - margin.bottom - yD3(d.y))
+      .attr('fill', (d) => colorScale(d.y))
+      .on('mousemove', (event, d) => {
+        if (tooltipRef.current) {
+          // Get bounding box of SVG to calculate relative positioning
+          const svgRect = svgRef.current?.getBoundingClientRect();
+          const horizontalOffset = 15;
+          const verticalOffset = 30;
+
+          // Calculate the position of the tooltip relative to the SVG
+          const tooltipX =
+            event.clientX - (svgRect?.left || 0) + horizontalOffset;
+          const tooltipY = event.clientY - (svgRect?.top || 0) - verticalOffset;
+
+          tooltipRef.current.style.left = `${tooltipX}px`;
+          tooltipRef.current.style.top = `${tooltipY}px`;
+          tooltipRef.current.style.opacity = '1';
+          tooltipRef.current.textContent = tooltipMapper!(d.x, d.y);
         }
 
-        // Check that x and y data has the same number of samples
-        if (x.length != y.length) {
-            throw new Error('X and Y data must have the same lenght');
+        // Highlight the hovered bar
+        d3.selectAll('rect').transition().duration(200).style('opacity', 0.4);
+
+        d3.select(event.target as SVGRectElement)
+          .transition()
+          .duration(200)
+          .style('opacity', 1);
+      })
+      .on('mouseleave', () => {
+        if (tooltipRef.current) {
+          tooltipRef.current.style.opacity = '0';
         }
 
-        // Clear the svg in case of re-rendering
-        d3.select(svgRef.current).selectAll('*').remove();
+        // Reset opacity for all bars
+        d3.selectAll('rect').transition().duration(200).style('opacity', 1);
+      });
+  }, [x, y]);
 
-        // Define the margin (used to make the svg do not clip to the border of the containing div)
-        const margin = {
-            top: mt || 20,
-            right: mr || 0,
-            bottom: mb || 40,
-            left: ml || 30
-        };
-
-        // Define the Y domain
-        var domain = [
-            yDomainMin || Math.min(0, Math.min(...y)),
-            yDomainMax || Math.max(...y)
-        ];
-
-        // The colorscale of the chart
-        const colorScale = d3.scaleSequential(colorInterpoaltor).domain(domain);
-
-        // Define current svg dimension and properties
-        const svg = d3
-            .select(svgRef.current)
-            .attr('width', width)
-            .attr('height', height)
-            .append('g')
-            .attr('transform', `translate(${margin.left},${margin.top})`);
-
-        // Define X and Y scales
-        const xD3 = d3
-            .scaleBand()
-            .domain(x)
-            .range([0, width - margin.left - margin.right])
-            .padding(0.2);
-
-        const yD3 = d3
-            .scaleLinear()
-            .domain(domain)
-            .nice()
-            .range([height - margin.top - margin.bottom, 0]);
-
-        // Define the X axis labels
-        svg
-            .append('g')
-            .attr('transform', `translate(0,${height - margin.top - margin.bottom})`)
-            .call(d3.axisBottom(xD3).tickSize(0))
-            .selectAll('text')
-            .attr('transform', 'rotate(-45)')
-            .style('text-anchor', 'end');
-
-        // Define the Y axis lables
-        var prefix = yLabelsPrefix || '';
-        var suffix = yLabelsSuffix || '';
-        svg
-            .append('g')
-            .call(d3.axisLeft(yD3).tickFormat((y) => `${prefix}${y}${suffix}`))
-            .append('text')
-            .attr('text-anchor', 'end')
-            .attr('fill', 'black')
-            .attr('font-weight', 'bold')
-            .attr('y', -10)
-            .attr('x', -10);
-
-        // Zip the X and Y values together
-        var data = y.map((value, index) => {
-            return {
-                x: x[index],
-                y: value
-            };
-        });
-
-        // Iterate over the data and create the svg bars and tooltips
-        tooltipMapper =
-            tooltipMapper || ((x, y) => `${x}: ${prefix}${y.toString()}${suffix}`);
-
-        svg
-            .selectAll('rect')
-            .data(data)
-            .enter()
-            .append('rect')
-            .attr('x', (d) => xD3(d.x) || 0)
-            .attr('y', (d) => yD3(d.y))
-            .attr('width', xD3.bandwidth())
-            .attr('height', (d) => height - margin.top - margin.bottom - yD3(d.y))
-            .attr('fill', (d) => colorScale(d.y))
-            .on('mousemove', (event, d) => {
-                if (tooltipRef.current) {
-                    // Get bounding box of SVG to calculate relative positioning
-                    const svgRect = svgRef.current?.getBoundingClientRect();
-                    const horizontalOffset = 15;
-                    const verticalOffset = 30;
-
-                    // Calculate the position of the tooltip relative to the SVG
-                    const tooltipX =
-                        event.clientX - (svgRect?.left || 0) + horizontalOffset;
-                    const tooltipY = event.clientY - (svgRect?.top || 0) - verticalOffset;
-
-                    tooltipRef.current.style.left = `${tooltipX}px`;
-                    tooltipRef.current.style.top = `${tooltipY}px`;
-                    tooltipRef.current.style.opacity = '1';
-                    tooltipRef.current.textContent = tooltipMapper!(d.x, d.y);
-                }
-
-                // Highlight the hovered bar
-                d3.selectAll('rect').transition().duration(200).style('opacity', 0.4);
-
-                d3.select(event.target as SVGRectElement)
-                    .transition()
-                    .duration(200)
-                    .style('opacity', 1);
-            })
-            .on('mouseleave', () => {
-                if (tooltipRef.current) {
-                    tooltipRef.current.style.opacity = '0';
-                }
-
-                // Reset opacity for all bars
-                d3.selectAll('rect').transition().duration(200).style('opacity', 1);
-            });
-    }, [x, y]);
-
-    return (
-        <div className="overflow-x-auto h-full w-fit">
-            <svg ref={svgRef} />
-            <div
-                ref={tooltipRef}
-                className="absolute py-1 px-2 text-sm bg-white rounded border-2 border-solid opacity-0 pointer-events-none border-primary"
-            />
-        </div>
-    );
+  return (
+    <div className="overflow-x-auto h-full w-fit">
+      <svg ref={svgRef} />
+      <div
+        ref={tooltipRef}
+        className="absolute py-1 px-2 text-sm bg-white rounded border-2 border-solid opacity-0 pointer-events-none border-primary"
+      />
+    </div>
+  );
 }
