7b9b09829dc09ca0320e5b56a0fa26f5d8f4b3c5
Alex Valle <alexvalle75@gmail.com>
2025-01-30 23:01:18 +0100

    New tooltip

---
+++ web-application/src/components/tooltip.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { cn } from '@/lib/utils';
+
+export interface TooltipProps {
+    children?: React.ReactNode;
+    className?: string;
+}
+
+const Tooltip = React.forwardRef<HTMLDivElement, TooltipProps>(
+    ({ children, className }, ref) => {
+        return (
+            <div
+                className={cn(
+                    'absolute p-4 bg-background text-foreground rounded-xl shadow-xl pointer-events-none border border-border',
+                    className
+                )}
+                ref={ref}
+            >
+                {children}
+            </div>
+        );
+    }
+);
+
+export default Tooltip;
