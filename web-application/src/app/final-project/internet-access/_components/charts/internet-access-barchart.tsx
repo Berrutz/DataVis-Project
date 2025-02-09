8f87b1a56becff5f304d1d7a54f412f5fc651094
Diego-Chiola <chioladiego@gmail.com>
2025-02-09 19:07:14 +0100

    Refactor, applied new functions to genearl charts

--- web-application/src/app/final-project/internet-access/_components/charts/internet-access-barchart.tsx
+++ web-application/src/app/final-project/internet-access/_components/charts/internet-access-barchart.tsx
@@ -1,12 +1,22 @@
+import ChartContainer from '@/components/chart-container';
 import BarChart from '@/components/charts/barchart';
-import { getStaticFile } from '@/utils/general';
+import { H3 } from '@/components/headings';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from '@/components/ui/select';
+import { Skeleton } from '@/components/ui/skeleton';
+import { useGetD3Csv } from '@/hooks/use-get-d3-csv';
+import { getUnique } from '@/utils/general';
 import * as d3 from 'd3';
 import { useEffect, useState } from 'react';
 
-interface Data {
-  country: string;
-  year: string;
-  value: number;
+interface BarchartDataWrap {
+  x: string[];
+  y: number[];
 }
 
 interface InternetAccessBarChartProps {
@@ -18,49 +28,75 @@ const InternetAccessBarChart: React.FC<InternetAccessBarChartProps> = ({
   newWidth,
   newHeight
 }) => {
-  const [csvData, setData] = useState<Data[]>([]);
-  const [selectedYear, setSelectedYear] = useState<string>('2022'); // Set default year here
-  const [x, setX] = useState<string[]>([]);
-  const [y, setY] = useState<number[]>([]);
+  const [selectedYear, setSelectedYear] = useState<string>();
+  const [barchartData, setBarchartData] = useState<BarchartDataWrap | null>(
+    null
+  );
 
-  // Fetch data when the component mounts
+  // Get the data from the csv file using D3
+  const csvData = useGetD3Csv(
+    'internet-access-level/internet-access.csv',
+    (d) => ({
+      country: d.Country,
+      year: +d.Year,
+      value: +d.Value
+    })
+  );
+
+  // Choose the default selection values when the csv is loaded and fullfilled
   useEffect(() => {
-    const fetchData = async () => {
-      const csvData = await d3.csv(
-        getStaticFile('/datasets/internet-access-level/internet-access.csv'),
-        (d) => ({
-          country: d.Country,
-          year: d.Year,
-          value: +d.Value
-        })
-      );
-      setData(csvData);
-    };
+    if (csvData === null || csvData.length <= 0) return;
 
-    fetchData();
-  }, []);
+    // As default choose the most recent year
+    const years = csvData.map((value) => value.year);
+    const selectedYear = Math.max(...years);
+
+    // Set the first default selection for the first barchart visualization
+    setSelectedYear(selectedYear.toString());
+  }, [csvData]);
 
   useEffect(() => {
-    // Filter data based on the selected year
-    const filteredData = csvData.filter((d) => d.year === selectedYear);
+    if (!selectedYear || csvData === null) return;
 
-    if (filteredData.length === 0) return;
+    const filteredData = csvData.filter((d) => d.year === +selectedYear);
 
     filteredData.sort((a, b) => b.value - a.value);
 
-    // Extract country names into an array
-    setX(filteredData.map((d) => d.country));
+    // Set X and Y data to be passed to the BarChart component
+    setBarchartData({
+      x: filteredData.map((d) => d.country),
+      y: filteredData.map((d) => d.value)
+    });
+  }, [selectedYear]);
 
-    // Extract values into an array
-    setY(filteredData.map((d) => d.value));
-  }, [csvData, newWidth, newHeight, selectedYear]);
-  if (csvData.length <= 0 || x.length <= 0 || y.length <= 0) return;
+  // The csv is not yet loaded or
+  // the default selection has not already initializated or
+  // neither one of the x value and y value state for the barchart has been initializated
+  if (!selectedYear || csvData === null || barchartData === null) {
+    return (
+      <ChartContainer>
+        <Skeleton className="w-full bg-gray-200 rounded-xl h-[500px]" />
+      </ChartContainer>
+    );
+  }
+
+  // The csv is loaded but no data has been found
+  if (csvData.length <= 0) {
+    throw Error('Cannot retrieve the data from the csv');
+  }
+
+  // Unique years to make the user input selections
+  const uniqueYears = getUnique(
+    csvData.map((value) => value.year),
+    (a, b) => b - a
+  );
 
   return (
-    <div className="flex flex-col justify-center items-center">
+    <ChartContainer className="flex flex-col gap-8">
+      <H3>Different countires compared by internet access level</H3>
       <BarChart
-        x={x}
-        y={y}
+        x={barchartData.x}
+        y={barchartData.y}
         width={newWidth}
         height={newHeight}
         colorInterpoaltor={d3.interpolateReds}
@@ -68,22 +104,27 @@ const InternetAccessBarChart: React.FC<InternetAccessBarChartProps> = ({
         mb={70}
         yLabelsSuffix="%"
       ></BarChart>
-      <div className="mt-3">
-        <label htmlFor="year">Select Year: </label>
-        <select
-          id="year"
-          value={selectedYear}
-          onChange={(e) => setSelectedYear(e.target.value)}
-          className="py-1 px-2 ml-2 rounded-md border bg-background"
-        >
-          {[...new Set(csvData.map((d) => d.year))].map((year) => (
-            <option key={year} value={year}>
-              {year}
-            </option>
-          ))}
-        </select>
+      <div className="flex flex-col gap-6 sm:flex-row">
+        <div className="sm:w-1/3">
+          <label>Year</label>
+          <Select
+            onValueChange={setSelectedYear}
+            defaultValue={selectedYear.toString()}
+          >
+            <SelectTrigger>
+              <SelectValue placeholder="Year" />
+            </SelectTrigger>
+            <SelectContent>
+              {uniqueYears.map((year) => (
+                <SelectItem key={year} value={year.toString()}>
+                  {year}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
       </div>
-    </div>
+    </ChartContainer>
   );
 };
 
--- web-application/src/components/charts/utils/facetedBarChart.ts
+++ web-application/src/components/charts/utils/facetedBarChart.ts
@@ -22,23 +22,3 @@ export function calculateMaxLength(width: number, fontSize: number): number {
   const charWidth = fontSize * 8; // Approximate character width based on font size
   return Math.floor(width / charWidth);
 }
-
-export function tooltipPositionOnMouseMove(
-  tooltipRef: React.MutableRefObject<HTMLDivElement | null>,
-  svgRef: React.MutableRefObject<SVGSVGElement | null>,
-  event: any,
-  horizontalOffset: number,
-  verticalOffset: number
-) {
-  const svgRect = svgRef.current?.getBoundingClientRect();
-
-  if (tooltipRef.current) {
-    const tooltipX = event.clientX - (svgRect?.left || 0) + horizontalOffset;
-    const tooltipY = event.clientY - (svgRect?.top || 0) - verticalOffset;
-
-    tooltipRef.current.style.left = `${tooltipX}px`;
-    tooltipRef.current.style.top = `${tooltipY}px`;
-    tooltipRef.current.style.display = 'block';
-    tooltipRef.current.style.opacity = '1';
-  }
-}
