54a9aacd394f277c3fd1e783eac8630afa64a386
Alex Valle <alexvalle75@gmail.com>
2025-03-23 18:41:13 +0100

    Updated linechart with real linechart

--- web-application/src/app/final-project/_components/digital-skills/_charts/linechart-year-comparison.tsx
+++ web-application/src/app/final-project/_components/digital-skills/_charts/linechart-year-comparison.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import BarChart, { Point } from '@/components/charts/barchart';
 import { Skeleton } from '@/components/ui/skeleton';
 import {
   Select,
@@ -15,11 +14,7 @@ import { foundOrFirst, getUnique } from '@/utils/general';
 import { useGetD3Csv } from '@/hooks/use-get-d3-csv';
 import ChartContainer from '@/components/chart-container';
 import { H3 } from '@/components/headings';
-
-type LinechartState = {
-  x: string[];
-  y: number[];
-};
+import LineChart, { Line } from '@/components/charts/linechart';
 
 export default function LinechartYearsDigitalSkills() {
   // Represent a selection for the user to switch the barchart parameters
@@ -27,10 +22,20 @@ export default function LinechartYearsDigitalSkills() {
   const [indicIs, setIndicIs] = useState<string>();
   const [indType, setIndType] = useState<string>();
 
-  // The current state of the barchart x and y
-  const [barchartState, setBarchartState] = useState<LinechartState | null>(
-    null
-  );
+  // I decided to create two linechart one for data before 2019 and one for data after 2019
+  // This because the methods used to derive the digital skills has been changed after 2019 and
+  // data is scaled in a different way. If all the period span is keept together the data could be
+  // misleading due to scales factors and different methods used to derive the digital skills.
+
+  // The current state of the linechart x and y values for data before 2019
+  const [linechartBefore2019Data, setLinechartBefore2019Data] = useState<
+    Line[] | null
+  >(null);
+
+  // The current state of the linechart x and y values for data after 2019
+  const [linechartAfter2019Data, setLinechartAfter2019Data] = useState<
+    Line[] | null
+  >(null);
 
   // Get the data from the csv file using D3
   const csvData = useGetD3Csv('digital-skills/ilods-final.csv', (d) => ({
@@ -45,19 +50,19 @@ export default function LinechartYearsDigitalSkills() {
   useEffect(() => {
     if (csvData === null || csvData.length <= 0) return;
 
-    // As default choose the most recent year
+    // As default choose Italy
     const countries = csvData.map((value) => value.country);
-    var selectedCountry = foundOrFirst('Italy', countries);
+    const selectedCountry = foundOrFirst('Italy', countries);
 
     // As default indic_is choose `Individuals with no overall digital skills` or the first one in alphabetical order
     const indicIsList = csvData.map((value) => value.indic_is);
-    var selectedIndicIs = foundOrFirst(
+    const selectedIndicIs = foundOrFirst(
       'Individuals with no overall digital skills',
       indicIsList
     );
 
     const indTypeList = csvData.map((value) => value.ind_type);
-    var selectedIndType = foundOrFirst('All individuals', indTypeList);
+    const selectedIndType = foundOrFirst('All individuals', indTypeList);
 
     // Set the first default selection for the first barchart visualization
     setCountry(selectedCountry);
@@ -74,14 +79,41 @@ export default function LinechartYearsDigitalSkills() {
         (value) =>
           value.country === country &&
           value.indic_is === indicIs &&
-          value.ind_type === indType
+          value.ind_type === indType &&
+          value.value > 0.0
       )
-      .sort((a, b) => b.year - a.year);
+      .sort((a, b) => a.year - b.year);
 
-    setBarchartState({
-      x: filteredData.map((value) => value.year.toString()),
-      y: filteredData.map((value) => value.value)
-    });
+    const before2019Data = filteredData.filter((value) => value.year < 2019);
+    const after2019Data = filteredData.filter((value) => value.year >= 2019);
+
+    const linesBefore2019: Line[] = [
+      {
+        x: before2019Data.map((value) => value.year.toString()),
+        y: before2019Data.map((value) => value.value),
+        scatter: false,
+        tag: country,
+        color: 'blue'
+      }
+    ];
+
+    const linesAfter2019: Line[] = [
+      {
+        x: after2019Data.map((value) => value.year.toString()),
+        y: after2019Data.map((value) => value.value),
+        scatter: false,
+        tag: country,
+        color: 'blue'
+      }
+    ];
+
+    setLinechartBefore2019Data(linesBefore2019);
+    setLinechartAfter2019Data(linesAfter2019);
+
+    // setLinechartBefore2019Data({
+    //   x: filteredData.map((value) => value.year.toString()),
+    //   y: filteredData.map((value) => value.value)
+    // });
   }, [country, indicIs, indType]);
 
   // The csv is not yet loaded or
@@ -92,7 +124,8 @@ export default function LinechartYearsDigitalSkills() {
     !indicIs ||
     !indType ||
     csvData === null ||
-    barchartState === null
+    linechartBefore2019Data === null ||
+    linechartAfter2019Data === null
   ) {
     return (
       <ChartContainer>
@@ -114,32 +147,32 @@ export default function LinechartYearsDigitalSkills() {
   return (
     <ChartContainer className="flex flex-col gap-8">
       <H3>Digital skills compared over the years</H3>
-      {barchartState.x.length <= 0 ? (
+      {linechartBefore2019Data.length <= 0 ? (
         <div className="flex justify-center items-center h-[400px] w-[min(100%,_900px)]">
           <h1 className="text-xl font-medium text-destructive">
             No data available
           </h1>
         </div>
       ) : (
-        <BarChart
-          x={barchartState.x}
-          y={barchartState.y}
-          width={900}
-          height={400}
-          colorInterpoaltor={d3.interpolateBlues}
-          mb={90}
-          mr={20}
-          mt={0}
-          tooltipMapper={(point: Point): React.ReactNode => (
-            <div>
-              {point.x}: {point.y}%
-              <br />
-              <p className="text-sm text-foreground/60">
-                Percentage of people
-              </p>{' '}
-            </div>
-          )}
-        />
+        <>
+          <h3 className="text-xl font-semibold mb-[-30px]">
+            Years before 2019
+          </h3>
+          <LineChart
+            width={900}
+            height={400}
+            data={linechartBefore2019Data}
+            ml={40}
+          />
+
+          <h3 className="text-xl font-semibold mb-[-30px]">Years after 2019</h3>
+          <LineChart
+            width={900}
+            height={400}
+            data={linechartAfter2019Data}
+            ml={40}
+          />
+        </>
       )}
       <div className="flex flex-col gap-6 sm:flex-row">
         <div className="sm:w-1/3">
