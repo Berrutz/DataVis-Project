147245264860780134be6e540431dfdc276bfcf6
Alex Valle <alexvalle75@gmail.com>
2025-03-23 19:26:18 +0100

    Changed linechart still need to change the countries selection

--- web-application/src/app/final-project/_components/digital-skills/_charts/linechart-year-comparison.tsx
+++ web-application/src/app/final-project/_components/digital-skills/_charts/linechart-year-comparison.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import chroma from 'chroma-js';
 import { Skeleton } from '@/components/ui/skeleton';
 import {
   Select,
@@ -8,7 +9,6 @@ import {
   SelectTrigger,
   SelectValue
 } from '@/components/ui/select';
-import * as d3 from 'd3';
 import React, { useEffect, useState } from 'react';
 import { foundOrFirst, getUnique } from '@/utils/general';
 import { useGetD3Csv } from '@/hooks/use-get-d3-csv';
@@ -17,8 +17,8 @@ import { H3 } from '@/components/headings';
 import LineChart, { Line } from '@/components/charts/linechart';
 
 export default function LinechartYearsDigitalSkills() {
-  // Represent a selection for the user to switch the barchart parameters
-  const [country, setCountry] = useState<string>();
+  // Represent a selection for the user to switch the linecharts parameters
+  const [countries, setCountries] = useState<string[]>();
   const [indicIs, setIndicIs] = useState<string>();
   const [indType, setIndType] = useState<string>();
 
@@ -50,9 +50,13 @@ export default function LinechartYearsDigitalSkills() {
   useEffect(() => {
     if (csvData === null || csvData.length <= 0) return;
 
-    // As default choose Italy
+    // As default choose italy, germany, france and romania as countries if they are present in the csv
     const countries = csvData.map((value) => value.country);
-    const selectedCountry = foundOrFirst('Italy', countries);
+    const allUniqueCountries = [...new Set(countries)];
+    const defaultCountries = ['Italy', 'Germany', 'France', 'Romania'];
+    const selectedCountries = defaultCountries.filter((value) =>
+      allUniqueCountries.includes(value)
+    );
 
     // As default indic_is choose `Individuals with no overall digital skills` or the first one in alphabetical order
     const indicIsList = csvData.map((value) => value.indic_is);
@@ -64,63 +68,83 @@ export default function LinechartYearsDigitalSkills() {
     const indTypeList = csvData.map((value) => value.ind_type);
     const selectedIndType = foundOrFirst('All individuals', indTypeList);
 
-    // Set the first default selection for the first barchart visualization
-    setCountry(selectedCountry);
+    // Set the first default selection for the first linechart visualization
+    setCountries(selectedCountries);
     setIndicIs(selectedIndicIs);
     setIndType(selectedIndType);
   }, [csvData]);
 
-  // Change the barchart state based on the current user selection
+  // Change the linechart state based on the current user selection
   useEffect(() => {
-    if (!country || !indicIs || !indType || csvData === null) return;
+    if (!countries || !indicIs || !indType || csvData === null) return;
 
     const filteredData = csvData
       .filter(
         (value) =>
-          value.country === country &&
+          countries.includes(value.country) &&
           value.indic_is === indicIs &&
-          value.ind_type === indType &&
-          value.value > 0.0
+          value.ind_type === indType
       )
       .sort((a, b) => a.year - b.year);
 
     const before2019Data = filteredData.filter((value) => value.year < 2019);
     const after2019Data = filteredData.filter((value) => value.year >= 2019);
 
-    const linesBefore2019: Line[] = [
-      {
-        x: before2019Data.map((value) => value.year.toString()),
-        y: before2019Data.map((value) => value.value),
-        scatter: false,
-        tag: country,
-        color: 'blue'
-      }
-    ];
+    console.table(before2019Data);
 
-    const linesAfter2019: Line[] = [
-      {
-        x: after2019Data.map((value) => value.year.toString()),
-        y: after2019Data.map((value) => value.value),
+    const uniqueCountriesBefore2019 = getUnique(
+      before2019Data.map((value) => value.country)
+    );
+    const uniqueCountriesAfter2019 = getUnique(
+      after2019Data.map((value) => value.country)
+    );
+
+    const colorsBefore2019 = chroma
+      .scale('Paired')
+      .colors(uniqueCountriesBefore2019.length);
+    const colorsAfter2019 = chroma
+      .scale('Paired')
+      .colors(uniqueCountriesAfter2019.length);
+
+    const linesBefore2019: Line[] = [];
+    for (let i = 0; i < uniqueCountriesBefore2019.length; i++) {
+      const country = uniqueCountriesBefore2019[i];
+      const filtered = before2019Data.filter(
+        (value) => value.country === country && value.value > 0.0
+      );
+      linesBefore2019.push({
+        x: filtered.map((value) => value.year.toString()),
+        y: filtered.map((value) => value.value),
         scatter: false,
         tag: country,
-        color: 'blue'
-      }
-    ];
+        color: colorsBefore2019[i]
+      });
+    }
+
+    const linesAfter2019: Line[] = [];
+    for (let i = 0; i < uniqueCountriesAfter2019.length; i++) {
+      const country = uniqueCountriesAfter2019[i];
+      const filtered = after2019Data.filter(
+        (value) => value.country === country && value.value > 0.0
+      );
+      linesAfter2019.push({
+        x: filtered.map((value) => value.year.toString()),
+        y: filtered.map((value) => value.value),
+        scatter: false,
+        tag: country,
+        color: colorsAfter2019[i]
+      });
+    }
 
     setLinechartBefore2019Data(linesBefore2019);
     setLinechartAfter2019Data(linesAfter2019);
-
-    // setLinechartBefore2019Data({
-    //   x: filteredData.map((value) => value.year.toString()),
-    //   y: filteredData.map((value) => value.value)
-    // });
-  }, [country, indicIs, indType]);
+  }, [countries, indicIs, indType]);
 
   // The csv is not yet loaded or
   // the default selection has not already initializated or
-  // neither one of the x value and y value state for the barchart has been initializated
+  // the value state for the linechart has been initializated
   if (
-    !country ||
+    !countries ||
     !indicIs ||
     !indType ||
     csvData === null ||
@@ -144,6 +168,8 @@ export default function LinechartYearsDigitalSkills() {
   const uniqueIndType = getUnique(csvData.map((value) => value.ind_type));
   const uniqueCountries = getUnique(csvData.map((value) => value.country));
 
+  const height = 500;
+
   return (
     <ChartContainer className="flex flex-col gap-8">
       <H3>Digital skills compared over the years</H3>
@@ -160,7 +186,7 @@ export default function LinechartYearsDigitalSkills() {
           </h3>
           <LineChart
             width={900}
-            height={400}
+            height={height}
             data={linechartBefore2019Data}
             ml={40}
           />
@@ -168,7 +194,7 @@ export default function LinechartYearsDigitalSkills() {
           <h3 className="text-xl font-semibold mb-[-30px]">Years after 2019</h3>
           <LineChart
             width={900}
-            height={400}
+            height={height}
             data={linechartAfter2019Data}
             ml={40}
           />
@@ -177,7 +203,7 @@ export default function LinechartYearsDigitalSkills() {
       <div className="flex flex-col gap-6 sm:flex-row">
         <div className="sm:w-1/3">
           <label>Year</label>
-          <Select onValueChange={setCountry} defaultValue={country}>
+          <Select onValueChange={setCountries} defaultValue={countries}>
             <SelectTrigger>
               <SelectValue placeholder="Country" />
             </SelectTrigger>
