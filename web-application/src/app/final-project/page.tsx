d7cfd3f4755989c61ebc74ee209afa10d7830467
Alex Valle <alexvalle75@gmail.com>
2025-03-24 23:02:32 +0100

    Removed unused imports and comments from final project page

--- web-application/src/app/final-project/page.tsx
+++ web-application/src/app/final-project/page.tsx
@@ -1,11 +1,5 @@
 'use client';
 
-import InternetAccessBarChart from './internet-access/_components/charts/internet-access-barchart';
-import InternetAccessFacetedBarChart from './internet-access/_components/charts/internet-access-faceted-barchart';
-import InternetAccessMap from './internet-access/_components/charts/internet-access-map';
-import InternetUseAlluvial from './internet-access/_components/charts/internet-use-alluvial';
-import InternetUseLineChart from './internet-access/_components/charts/internet-use-linechart';
-
 /*
  * Final Project:
  * The idea is to create 3 folders under this `final-project` folder that subdivide the
@@ -26,14 +20,7 @@ import InternetUseLineChart from './internet-access/_components/charts/internet-
  *
  * Or you can do whatever you want and make this more messy (o_o)
  */
-
-import Useoftheinternet from './use-of-the-internet/Useoftheinternet';
-import ComputerUseAlluvial from './internet-access/_components/charts/computer-use-alluvial';
-import StackedBarChartTest from './internet-access/_components/charts/stacked-barchart-test';
-import MapContainer from '@/components/map-switch-container';
-import { getStaticFile } from '@/utils/general';
 import Arguments from './_components/arguments';
-import { H1, H2 } from '@/components/headings';
 import HeroFinalProject from './_components/hero';
 import IntroductionFinalProject from './_components/introduction';
 
@@ -43,31 +30,6 @@ export default function FinalProject() {
       <HeroFinalProject />
       <IntroductionFinalProject />
       <Arguments />
-      {/*       <div className="relative">
-        <MapContainer
-          className="sm:relative overflow-hidden"
-          components={[
-            {
-              buttonText: 'Bars',
-              component: (
-                <InternetAccessBarChart
-                  newWidth={800}
-                  newHeight={550}
-                ></InternetAccessBarChart>
-              )
-            },
-            {
-              buttonText: 'Map',
-              component: (
-                <InternetAccessMap
-                  newWidth={800}
-                  newHeight={600}
-                ></InternetAccessMap>
-              )
-            }
-          ]}
-        ></MapContainer>
-      </div> */}
     </main>
   );
 }
