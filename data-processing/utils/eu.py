b6100ad45d0658f87f06a2bde7a12d69cc0c10a5
Alex Valle <alexvalle75@gmail.com>
2025-03-27 19:34:34 +0100

    Added util function to remove non eu countries from dataframes

---
+++ data-processing/utils/eu.py
@@ -0,0 +1,52 @@
+from typing import overload
+
+from pandas import DataFrame, Series
+
+EU_COUNTRIES = [
+    "Austria",
+    "Belgium",
+    "Bulgaria",
+    "Croatia",
+    "Cyprus",
+    "Czech Republic",
+    "Denmark",
+    "Estonia",
+    "Finland",
+    "France",
+    "Germany",
+    "Greece",
+    "Hungary",
+    "Ireland",
+    "Italy",
+    "Latvia",
+    "Lithuania",
+    "Luxembourg",
+    "Malta",
+    "Netherlands",
+    "Poland",
+    "Portugal",
+    "Romania",
+    "Slovakia",
+    "Slovenia",
+    "Spain",
+    "Sweden",
+    "United Kingdom",
+]
+
+
+def remove_non_eu_countires(dataframe: DataFrame, geo_column_name: str) -> DataFrame:
+    """
+    Filter out non eu countries from the dataframe given the column that identifies the
+    geographical information (aka country name).
+
+    :param dataframe: The dataframe to be filtered.
+    :param geo_column_name: The column name that identifies the geographical information.
+    :return: The dataframe filtered without the non eu countries.
+    """
+
+    mask = dataframe[geo_column_name].isin(EU_COUNTRIES)
+    df_filtered = dataframe[mask]
+
+    if not isinstance(df_filtered, DataFrame):
+        raise Exception("Canno remove the non eu countries")
+    return df_filtered
