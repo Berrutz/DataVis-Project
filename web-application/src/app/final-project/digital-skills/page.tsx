141d8e94ef5f3b55648ec086bb0b8c6a095b0e9c
Alex Valle <alexvalle75@gmail.com>
2025-04-23 18:05:19 +0200

    Removed digital skills linechart

--- web-application/src/app/final-project/digital-skills/page.tsx
+++ web-application/src/app/final-project/digital-skills/page.tsx
@@ -1,6 +1,5 @@
 import { H1, H2 } from '@/components/headings';
 import BarchartCountriesDigitalSkills from './_components/barchart-contry-digital-skills';
-import LinechartYearsDigitalSkills from './_components/linechart-year-comparison';
 import StackedBarchartICTEducationBySex from './_components/stacked-barchart-ict-education-by-sex';
 import StackedBarChartAgeDigitalSkills from './_components/stacked-bar-chart-digital-skill-age';
 import FinalPageSectionsNav from '../_components/final-page-sections-nav';
@@ -26,11 +25,6 @@ export default function DigitalSkills() {
               Different countires compared by digital skill levels
             </H2>
             <BarchartCountriesDigitalSkills />
-          </div>
-
-          <div>
-            <H2 className="mb-8">Digital skills compared over the years</H2>
-            <LinechartYearsDigitalSkills />
           </div>
 
           <div>
