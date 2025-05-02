3d00b32b7434005d9a159689552867002a242a85
Alex Valle <alexvalle75@gmail.com>
2025-05-02 20:39:40 +0200

    Fixed dialog now closing when clicking outside of it

--- web-application/src/components/modal-dialog.tsx
+++ web-application/src/components/modal-dialog.tsx
@@ -1,5 +1,5 @@
 import { cn } from '@/lib/utils';
-import { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Button } from './ui/button';
 import { X } from 'lucide-react';
 
@@ -8,59 +8,63 @@ export interface ModalDialogProps
   onModalClosing?: Function;
 }
 
-export default function ModalDialog({
-  className,
-  children,
-  open,
-  ...props
-}: ModalDialogProps) {
-  const ref = useRef<HTMLDialogElement | null>(null);
+const ModalDialog = React.forwardRef<HTMLDivElement, ModalDialogProps>(
+  ({ className, children, open, ...props }, ref) => {
+    const dialogRef = useRef<HTMLDialogElement | null>();
 
-  useEffect(() => {
-    if (open) {
-      document.body.style.overflow = 'hidden';
-    }
+    useEffect(() => {
+      if (!dialogRef) return;
 
-    ref.current?.addEventListener('close', closeModal);
-    return () => {
-      ref.current?.removeEventListener('close', closeModal);
+      if (open) {
+        document.body.style.overflow = 'hidden';
+      }
+
+      dialogRef.current?.addEventListener('close', closeModal);
+      return () => {
+        dialogRef.current?.removeEventListener('close', closeModal);
+      };
+    }, [open]);
+
+    const closeModal = () => {
+      if (props.onModalClosing !== undefined) {
+        props.onModalClosing();
+      }
+
+      if (dialogRef === null) return;
+      document.body.style.overflow = '';
+      dialogRef.current?.close();
     };
-  }, [open]);
 
-  const closeModal = () => {
-    if (props.onModalClosing !== undefined) {
-      props.onModalClosing();
-    }
-
-    if (ref === null) return;
-    document.body.style.overflow = '';
-    ref.current?.close();
-  };
-
-  return (
-    <dialog
-      ref={ref}
-      open={open}
-      className={cn(
-        'p-3 fixed top-0 left-0 w-full h-full z-[9999] bg-background/80',
-        className
-      )}
-    >
-      <div className="flex justify-center items-center size-full">
-        <div className="shadow-md p-3 rounded-xl border bg-background w-[min(800px,_100%)]">
-          <div className="flex justify-end w-full">
-            <Button
-              variant={'ghost'}
-              onClick={closeModal}
-              size={'icon'}
-              className="rounded-[calc(theme(borderRadius.xl)_-_1px)]"
-            >
-              <X />
-            </Button>
+    return (
+      <dialog
+        ref={dialogRef}
+        open={open}
+        className={cn(
+          'p-3 fixed top-0 left-0 w-full h-full z-[9999] bg-background/80',
+          className
+        )}
+      >
+        <div className="flex justify-center items-center size-full">
+          <div
+            ref={ref}
+            className="shadow-md p-3 rounded-xl border bg-background w-[min(800px,_100%)]"
+          >
+            <div className="flex justify-end w-full">
+              <Button
+                variant={'ghost'}
+                onClick={closeModal}
+                size={'icon'}
+                className="rounded-[calc(theme(borderRadius.xl)_-_1px)]"
+              >
+                <X />
+              </Button>
+            </div>
+            {children}
           </div>
-          {children}
         </div>
-      </div>
-    </dialog>
-  );
-}
+      </dialog>
+    );
+  }
+);
+
+export default ModalDialog;
